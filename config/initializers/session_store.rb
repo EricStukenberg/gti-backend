8944254a0da7abc47825fe01cc84479ab22c6245
stukene <ericstukenberg@outlook.com>
2020-03-18 16:19:18 -0700

    Auth again

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: 'gti-backend', domain: 'gti-frontend'
+    Rails.application.config.session_store :cookie_store, key: '_gti-backend', domain: 'gti-frontend'
   else
-    Rails.application.config.session_store :cookie_store, key: 'gti-backend' 
+    Rails.application.config.session_store :cookie_store, key: '_gti-backend' 
   end
