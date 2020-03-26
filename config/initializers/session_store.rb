f7682a42a1943a089efadc7adec3ff25ea242dcc
stukene <ericstukenberg@outlook.com>
2020-03-26 14:10:14 -0700

    Cors issue Fixed

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
-if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_gti-backend', domain: 'gti-frontend'
-  else
-    Rails.application.config.session_store :cookie_store, key: '_gti-backend' 
-  end
+# if Rails.env === 'production' 
+#     Rails.application.config.session_store :cookie_store, key: '_gti-backend', domain: 'gti-frontend'
+#   else
+#     Rails.application.config.session_store :cookie_store, key: '_gti-backend' 
+#   end
